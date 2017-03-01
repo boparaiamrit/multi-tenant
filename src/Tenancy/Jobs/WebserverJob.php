6dd91fa748927299889205e2e91bc9fad7ac6a27
Nitish <Nthakur1990@gmail.com>
2017-03-01 19:14:28 +0530

    ttenancy webserver job

--- src/Tenancy/Jobs/WebserverJob.php
+++ src/Tenancy/Jobs/WebserverJob.php
@@ -1,27 +1,37 @@
 <?php
 
-namespace Boparaiamrit\Tenancy;
+namespace Boparaiamrit\Tenancy\Jobs;
 
 
 use Boparaiamrit\Webserver\Generators\Webserver\Env;
 use Boparaiamrit\Webserver\Generators\Webserver\Fpm;
 use Boparaiamrit\Webserver\Generators\Webserver\Nginx;
 use Boparaiamrit\Webserver\Generators\Webserver\Supervisor;
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Console\Kernel;
+use Illuminate\Contracts\Foundation\Application;
 use Illuminate\Contracts\Queue\ShouldQueue;
-use Illuminate\Queue\SerializesModels;
-use Promoto\Jobs\Job;
+use Illuminate\Queue\InteractsWithQueue;
+use Jenssegers\Mongodb\Eloquent\Model;
 
-class Webserver extends Job implements ShouldQueue
+class WebserverJob implements ShouldQueue
 {
-	use SerializesModels;
+	use InteractsWithQueue, Queueable;
 	
+	/**
+	 * @var Model
+	 */
 	protected $Host;
 	
-	public function __construct($Host)
+	/**
+	 * @var Model
+	 */
+	protected $user = null;
+	
+	public function __construct($Host, $user = [])
 	{
 		$this->Host = $Host;
-		
-		$this->onQueue('system');
+		$this->user = $user;
 	}
 	
 	public function handle()
@@ -30,11 +40,31 @@ class Webserver extends Job implements ShouldQueue
 		
 		// Php FPM
 		(new Fpm($this->Host))->onCreate();
+		
 		// Supervisor
 		(new Supervisor($this->Host))->onCreate();
+		
 		// Webservers
 		(new Nginx($this->Host))->onCreate();
+		
 		// Env
 		(new Env($this->Host))->onCreate();
+		
+		app(Kernel::class)->call('db:seed', [
+			'--force' => true, '--hostname' => $this->Host->identifier
+		]);
+		
+		app(Kernel::class)->call('setup:admin', [
+			'--email' => array_get($this->user, 'email'),
+			'--name'  => array_get($this->user, 'name'),
+			'--hostname' => $this->Host->hostname
+		]);
+		
+		app(Kernel::class)->call('setup:email', [
+			'--email' => array_get($this->user, 'email'),
+			'--hostname' => $this->Host->hostname
+		]);
+		
 	}
+	
 }
