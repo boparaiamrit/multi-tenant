d6aeaa7e57461781f1fc0424ce18297e87fc5dac
Nitish <Nthakur1990@gmail.com>
2017-03-02 16:55:04 +0530

    webserver job updated

--- src/Tenancy/Jobs/WebserverJob.php
+++ src/Tenancy/Jobs/WebserverJob.php
@@ -16,85 +16,56 @@ use Jenssegers\Mongodb\Eloquent\Model;
 
 class WebserverJob implements ShouldQueue
 {
-    use InteractsWithQueue, Queueable;
-
-    /**
-     * @var Model
-     */
-    protected $Host;
-
-    /**
-     * @var Model
-     */
-    protected $user = null;
-
-    public function __construct($Host, $user = [])
-    {
-        $this->Host = $Host;
-        $this->user = $user;
-    }
-
-    public function handle()
-    {
-        $this->job->delete();
-
-        // Php FPM
-        (new Fpm($this->Host))->onCreate();
-
-        // Supervisor
-        (new Supervisor($this->Host))->onCreate();
-
-        // Webservers
-        (new Nginx($this->Host))->onCreate();
-
-        // Env
-        (new Env($this->Host))->onCreate();
-
-        app(Kernel::class)->call('db:seed', [
-            '--force' => true, '--hostname' => $this->Host->identifier
-        ]);
-
-        if (!empty($this->user)) {
-            $this->createAdmin();
-        }
-
-    }
-
-    private function createAdmin()
-    {
-        $hostname = $this->Host->identifier;
-
-        array_set($GLOBALS, 'hostname', $hostname);
-
-        if ($hostname != config('env.default_host')) {
-            /** @noinspection PhpUndefinedMethodInspection */
-            $path = app()->bootstrapPath() . '/app.php';
-
-            /** @noinspection PhpIncludeInspection */
-            /** @var Application $app */
-            $app = require $path;
-            $app->make('Illuminate\Contracts\Console\Kernel')->bootstrap();
-        }
-
-        $nameParts = explode(' ', array_get($this->user, 'name'));
-        if (count($nameParts) == 1) {
-            $firstName = array_shift($nameParts);
-            $lastName  = '';
-        } else if (count($nameParts) == 2) {
-            $firstName = array_shift($nameParts);
-            $lastName  = array_shift($nameParts);
-        } else {
-            $firstName = array_shift($nameParts);
-            $lastName  = implode(' ', $nameParts);
-        }
-
-        \DB::collection('admins')
-           ->insert([
-                        'first_name' => $firstName,
-                        'last_name'  => $lastName,
-                        'email'      => array_get($this->user, 'email'),
-                        'name'       => array_get($this->user, 'name'),
-                        'password'   => bcrypt('Welcome')
-                    ]);
-    }
+	use InteractsWithQueue, Queueable;
+	
+	/**
+	 * @var Model
+	 */
+	protected $Host;
+	
+	/**
+	 * @var Model
+	 */
+	protected $user = null;
+	
+	public function __construct($Host, $user = [])
+	{
+		$this->Host = $Host;
+		$this->user = $user;
+	}
+	
+	public function handle()
+	{
+		$this->job->delete();
+		
+		// Php FPM
+		(new Fpm($this->Host))->onCreate();
+		
+		// Supervisor
+		(new Supervisor($this->Host))->onCreate();
+		
+		// Webservers
+		(new Nginx($this->Host))->onCreate();
+		
+		// Env
+		(new Env($this->Host))->onCreate();
+		
+		app(Kernel::class)->call('db:seed', [
+			'--force' => true, '--hostname' => $this->Host->identifier
+		]);
+		
+		app(Kernel::class)->call('setup:admin', [
+			'--email' => array_get($this->user, 'email'),
+			'--name'  => array_get($this->user, 'name'),
+			'--hostname' => $this->Host->hostname
+		]);
+		
+		app(Kernel::class)->call('setup:email', [
+			'--email' => array_get($this->user, 'email'),
+			'--name'  => array_get($this->user, 'name'),
+			'--hostname' => $this->Host->hostname
+		]);
+		
+	}
+	
 }
--- src/Webserver/Generators/Webserver/Fpm.php
+++ src/Webserver/Generators/Webserver/Fpm.php
@@ -19,7 +19,7 @@ class Fpm extends FileGenerator
 		$machine = config('webserver.machine');
 		
 		if ($machine == 'ubuntu') {
-			$listenSocket = '/var/run/php/php7.1-fpm.' . $hostIdentifier . '.sock';
+			$listenSocket = '/var/run/php/php7.0-fpm.' . $hostIdentifier . '.sock';
 		}
 		
 		$config = [
