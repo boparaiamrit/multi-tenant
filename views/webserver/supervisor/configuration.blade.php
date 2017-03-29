239b9bb930160464635d6205fdb22c79bd731ee0
Amritpal Singh <boparaiamrit@gmail.com>
2017-03-29 19:18:53 +0530

    Changed to Redis

--- views/webserver/supervisor/configuration.blade.php
+++ views/webserver/supervisor/configuration.blade.php
@@ -1,5 +1,5 @@
 [program:{!! $host_identifier !!}]
-command={!! $php_path !!} {!! $base_path !!}/artisan queue:work beanstalkd --daemon --sleep=3 --hostname={!! $host_identifier !!} --queue="{!! $host_identifier !!}"
+command={!! $php_path !!} {!! $base_path !!}/artisan queue:work redis --daemon --sleep=3 --hostname={!! $host_identifier !!} --queue="{!! $host_identifier !!}"
 autostart=true
 autoreload=true
 user={{ $user }}
@@ -7,7 +7,7 @@ redirect_stderr=true
 stdout_logfile={{ $base_path }}/storage/logs/supervisor-{!! $host_identifier !!}.log
 
 [program:{!! $host_identifier !!}.gravatar]
-command={!! $php_path !!} {!! $base_path !!}/artisan queue:work beanstalkd --daemon --sleep=3 --hostname={!! $host_identifier !!} --queue="{!! $host_identifier !!}.gravatar"
+command={!! $php_path !!} {!! $base_path !!}/artisan queue:work redis --daemon --sleep=3 --hostname={!! $host_identifier !!} --queue="{!! $host_identifier !!}.gravatar"
 autostart=true
 autoreload=true
 user={{ $user }}
@@ -15,7 +15,7 @@ redirect_stderr=true
 stdout_logfile={{ $base_path }}/storage/logs/supervisor-{!! $host_identifier !!}.log
 
 [program:{!! $host_identifier !!}.twitter]
-command={!! $php_path !!} {!! $base_path !!}/artisan queue:work beanstalkd --daemon --sleep=3 --hostname={!! $host_identifier !!} --queue="{!! $host_identifier !!}.twitter"
+command={!! $php_path !!} {!! $base_path !!}/artisan queue:work redis --daemon --sleep=3 --hostname={!! $host_identifier !!} --queue="{!! $host_identifier !!}.twitter"
 autostart=true
 autoreload=true
 user={{ $user }}
@@ -23,7 +23,7 @@ redirect_stderr=true
 stdout_logfile={{ $base_path }}/storage/logs/supervisor-{!! $host_identifier !!}.log
 
 [program:{!! $host_identifier !!}.activity]
-command={!! $php_path !!} {!! $base_path !!}/artisan queue:work beanstalkd --daemon --sleep=3 --hostname={!! $host_identifier !!} --queue="{!! $host_identifier !!}.activity"
+command={!! $php_path !!} {!! $base_path !!}/artisan queue:work redis --daemon --sleep=3 --hostname={!! $host_identifier !!} --queue="{!! $host_identifier !!}.activity"
 autostart=true
 autoreload=true
 user={{ $user }}
@@ -31,7 +31,7 @@ redirect_stderr=true
 stdout_logfile={{ $base_path }}/storage/logs/supervisor-{!! $host_identifier !!}.log
 
 [program:{!! $host_identifier !!}.notification]
-command={!! $php_path !!} {!! $base_path !!}/artisan queue:work beanstalkd --daemon --sleep=3 --hostname={!! $host_identifier !!} --queue="{!! $host_identifier !!}.notification"
+command={!! $php_path !!} {!! $base_path !!}/artisan queue:work redis --daemon --sleep=3 --hostname={!! $host_identifier !!} --queue="{!! $host_identifier !!}.notification"
 autostart=true
 autoreload=true
 user={{ $user }}
